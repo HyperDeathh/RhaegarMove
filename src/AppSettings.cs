8f985294d489841358c770b8eb7ba855412f51c1
Yunus Emre Birben <62476124+HyperDeathh@users.noreply.github.com>
2026-06-27 21:25:57 +0300

    config: add sticky resize setting

--- src/AppSettings.cs
+++ src/AppSettings.cs
@@ -20,6 +20,7 @@ namespace RhaegarMove
         public int AeroMaxSpeed = 65535;
         public int AeroSpeedTau = 64;
         public bool SnapToWindows = true;
+        public bool StickyResize = false;
         public bool AllowFullScreenWindows = false;
         public bool SkipMaximizedWindows = false;
         public bool NotifyMoveSizeEvents = true;
@@ -59,6 +60,7 @@ namespace RhaegarMove
             else if (key.Equals("AeroMaxSpeed", StringComparison.OrdinalIgnoreCase)) AeroMaxSpeed = ToInt(value, AeroMaxSpeed);
             else if (key.Equals("AeroSpeedTau", StringComparison.OrdinalIgnoreCase)) AeroSpeedTau = ToInt(value, AeroSpeedTau);
             else if (key.Equals("SnapToWindows", StringComparison.OrdinalIgnoreCase)) SnapToWindows = ToBool(value, SnapToWindows);
+            else if (key.Equals("StickyResize", StringComparison.OrdinalIgnoreCase)) StickyResize = ToBool(value, StickyResize);
             else if (key.Equals("AllowFullScreenWindows", StringComparison.OrdinalIgnoreCase)) AllowFullScreenWindows = ToBool(value, AllowFullScreenWindows);
             else if (key.Equals("SkipMaximizedWindows", StringComparison.OrdinalIgnoreCase)) SkipMaximizedWindows = ToBool(value, SkipMaximizedWindows);
             else if (key.Equals("NotifyMoveSizeEvents", StringComparison.OrdinalIgnoreCase)) NotifyMoveSizeEvents = ToBool(value, NotifyMoveSizeEvents);
