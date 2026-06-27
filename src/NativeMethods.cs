e9dbb2af23efdb493f261430546b29e3ef486e02
Yunus Emre Birben <62476124+HyperDeathh@users.noreply.github.com>
2026-06-27 20:46:00 +0300

    refactor: add native win32 interop layer

---
+++ src/NativeMethods.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RhaegarMove
+{
+    internal enum OperationKind
+    {
+        None = 0,
+        Move = 1,
+        Resize = 2
+    }
+
+    internal enum MouseButton
+    {
+        None = 0,
+        Left = 1,
+        Right = 2
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct POINT
+    {
+        public int x;
+        public int y;
+
+        public POINT(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+        public int Width { get { return right - left; } }
+        public int Height { get { return bottom - top; } }
+        public bool IsEmpty { get { return Width <= 0 || Height <= 0; } }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public uint dwFlags;
+    }
+
+    internal delegate IntPtr LowLevelProc(int nCode, IntPtr wParam, IntPtr lParam);
+    internal delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+    internal static class NativeMethods
+    {
+        public const int WH_MOUSE_LL = 14;
+        public const int HC_ACTION = 0;
+
+        public const int WM_MOUSEMOVE = 0x0200;
+        public const int WM_LBUTTONDOWN = 0x0201;
+        public const int WM_LBUTTONUP = 0x0202;
+        public const int WM_RBUTTONDOWN = 0x0204;
+        public const int WM_RBUTTONUP = 0x0205;
+
+        public const int VK_MENU = 0x12;
+        public const int VK_LMENU = 0xA4;
+        public const int VK_RMENU = 0xA5;
+        public const int VK_SHIFT = 0x10;
+        public const int VK_CONTROL = 0x11;
+
+        public const uint GA_ROOT = 2;
+        public const int SW_RESTORE = 9;
+        public const int SW_MAXIMIZE = 3;
+
+        public const uint WM_ENTERSIZEMOVE = 0x0231;
+        public const uint WM_EXITSIZEMOVE = 0x0232;
+        public const uint WM_SIZING = 0x0214;
+
+        public const int WMSZ_LEFT = 1;
+        public const int WMSZ_RIGHT = 2;
+        public const int WMSZ_TOP = 3;
+        public const int WMSZ_TOPLEFT = 4;
+        public const int WMSZ_TOPRIGHT = 5;
+        public const int WMSZ_BOTTOM = 6;
+        public const int WMSZ_BOTTOMLEFT = 7;
+        public const int WMSZ_BOTTOMRIGHT = 8;
+
+        public const uint SWP_NOSIZE = 0x0001;
+        public const uint SWP_NOMOVE = 0x0002;
+        public const uint SWP_NOZORDER = 0x0004;
+        public const uint SWP_NOACTIVATE = 0x0010;
+        public const uint SWP_NOOWNERZORDER = 0x0200;
+        public const uint SWP_ASYNCWINDOWPOS = 0x4000;
+
+        public const uint SMTO_ABORTIFHUNG = 0x0002;
+
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+        public const long WS_CAPTION = 0x00C00000L;
+        public const long WS_THICKFRAME = 0x00040000L;
+        public const long WS_CHILD = 0x40000000L;
+        public const long WS_DISABLED = 0x08000000L;
+        public const long WS_EX_TOOLWINDOW = 0x00000080L;
+        public const long WS_EX_NOACTIVATE = 0x08000000L;
+
+        public const int MONITOR_DEFAULTTONEAREST = 2;
+        public const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+
+        public const uint EVENT_SYSTEM_MOVESIZESTART = 0x000A;
+        public const uint EVENT_SYSTEM_MOVESIZEEND = 0x000B;
+
+        public static IntPtr GetWindowLongPtrSafe(IntPtr hwnd, int index)
+        {
+            if (IntPtr.Size == 8)
+                return GetWindowLongPtr64(hwnd, index);
+            return new IntPtr(GetWindowLong32(hwnd, index));
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(POINT point);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetAncestor(IntPtr hwnd, uint gaFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT rect);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsIconic(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsZoomed(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hwnd, int cmd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hwnd, IntPtr after, int x, int y, int width, int height, uint flags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hwnd, StringBuilder className, int maxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hwnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint processId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessageTimeout(IntPtr hwnd, uint msg, IntPtr wParam, ref RECT lParam, uint flags, uint timeout, out IntPtr result);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr MonitorFromPoint(POINT pt, uint flags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint flags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetMonitorInfo(IntPtr monitor, ref MONITORINFO info);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmGetWindowAttribute(IntPtr hwnd, int attribute, out RECT rect, int size);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool SetProp(IntPtr hwnd, string name, IntPtr data);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetProp(IntPtr hwnd, string name);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr RemoveProp(IntPtr hwnd, string name);
+
+        [DllImport("user32.dll")]
+        public static extern void NotifyWinEvent(uint eventType, IntPtr hwnd, int idObject, int idChild);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        private static extern int GetWindowLong32(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+        private static extern IntPtr GetWindowLongPtr64(IntPtr hwnd, int nIndex);
+    }
+}
